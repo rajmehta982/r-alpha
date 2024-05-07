bde3928783823bbbb4def3c5770ba6769271334f
raj.mehta@morningstar.com <rajmehta982@gmail.com>
2024-05-07 09:13:04 +0000

    live tested momentum strat

--- momentum.py
+++ momentum.py
@@ -1,3 +1,6 @@
+import yfinance as yf
+from datetime import datetime, timedelta
+
 def get_top_nse_stocks_by_volume(instrument_list, last_n_days=4):
     # Get NSE tickers
     
@@ -27,15 +30,13 @@ def get_top_nse_stocks_by_volume(instrument_list, last_n_days=4):
     
     return top_10_stocks
 
-ticker_ltp = {}
-position = 0
-momentum_short_window = 3
-momentum_long_window = 9
-
-trades = {}
-
 
 def momentum_strategy(tick):
+
+    global position
+    global trades
+    global capital
+    global ticker_ltp
     ### add ltp and return to dictionary
     token = tick['token']
     ltp = tick['last_traded_price']/100
@@ -44,26 +45,35 @@ def momentum_strategy(tick):
 
     if token in ticker_ltp:
         ticker_ltp[token]['ltp'].append(ltp)
-        ticker_ltp[token]['momentum_short'] = ticker_ltp[token]['ltp'].rolling(window=momentum_short_window).mean()
-        ticker_ltp[token]['momentum_long'] = ticker_ltp[token]['ltp'].rolling(window=momentum_long_window).mean()
+        momentum_short = pd.Series(ticker_ltp[token]['ltp']).rolling(window=momentum_short_window).mean().iloc[-1]
+
+        ticker_ltp[token]['momentum_short'].append(momentum_short)
+        momentum_long = pd.Series(ticker_ltp[token]['ltp']).rolling(window=momentum_long_window).mean().iloc[-1]
+        ticker_ltp[token]['momentum_long'].append(momentum_long)
     else:
         ticker_ltp[token] = {
             'ltp': [ltp],
-            'momentum_short':None,
-            'momentum_long':None
+            'momentum_short':[None],
+            'momentum_long': [None]
         }
 
+    # print(ticker_ltp[token]['momentum_short'])
+
     # Check momentum signal
-    if ticker_ltp[token]['momentum_short'][-1] > ticker_ltp[token]['momentum_long'][-1]:
-        momentum_signal = 1
-    elif ticker_ltp[token]['momentum_short'][-1] < ticker_ltp[token]['momentum_long'][-1]:
-        momentum_signal = -1
+    if (ticker_ltp[token]['momentum_short'][-1] is not None) and (ticker_ltp[token]['momentum_long'][-1] is not None):
+        if ticker_ltp[token]['momentum_short'][-1] > ticker_ltp[token]['momentum_long'][-1]:
+            momentum_signal = 1
+        elif ticker_ltp[token]['momentum_short'][-1] < ticker_ltp[token]['momentum_long'][-1]:
+            momentum_signal = -1
 
     #create trade
+    # print(momentum_signal)
+
     if momentum_signal == 1 and position == 0:
-        quantity = capital // ltp
+        quantity = capital / ltp
         price = ltp
         position = 1
+        capital = 0
 
         if token in trades:
             trades[token]['buy_prices'].append(price)
@@ -72,15 +82,17 @@ def momentum_strategy(tick):
         
         else:
             trades[token] = {
-            'buy_prices': price,
-            'quantity':quantity,
-            'sell_price':None
+            'buy_prices': [price],
+            'quantity': [quantity],
+            'sell_price': [None]
             }
 
     elif momentum_signal == -1 and position == 1:
         quantity = trades[token]['quantity'][-1]
         price = ltp
         position = 0
+
+        capital = quantity*price
         if token in trades:
             trades[token]['sell_price'].append(price)
         
